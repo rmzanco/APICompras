d078faf16bfbbe51036790a23e4f5780f1ea2157
Rodrigo Zanco <rmzancs@gmail.com>
2020-11-23 12:28:13 -0300

    Adicionar arquivos de projeto.

---
+++ APICompras/Controllers/ComprasController.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using APICompras.Models;
+using APICompras.Services;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace APICompras.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ComprasController : ControllerBase
+    {
+        private readonly ICompraService _service;
+
+        public ComprasController(ICompraService service)
+        {
+            _service = service;
+        }
+
+        //GET api/compras/
+        [HttpGet]
+        public ActionResult<IEnumerable<CompraItem>> Get()
+        {
+            var items = _service.GetAllItens();
+            return Ok(items);
+        }
+
+        //GET api/compras/5
+        [HttpGet("{id}")]
+        public ActionResult<CompraItem> Get(int id)
+        {
+            var item = _service.GetById(id);
+
+            if (item == null)
+                return NotFound();
+
+            return Ok(item);
+        }
+
+        //POST api/compras
+        [HttpPost]
+        public ActionResult Post([FromBody] CompraItem value)
+        {
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
+
+            var item = _service.Add(value);
+            return CreatedAtAction("Get", new { id = item.Id }, item);
+        }
+
+        //DELETE api/compras/5
+        [HttpDelete("{id}")]
+        public ActionResult Remove(int id)
+        {
+            var existingItem = _service.GetById(id);
+
+            if (existingItem == null)
+                return NotFound();
+
+            _service.Remove(id);
+            return Ok();
+        }
+
+    }
+}

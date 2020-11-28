e6fbad9d5aeee05618ae3344044344695e9be154
Rodrigo Zanco <rmzancs@gmail.com>
2020-11-28 01:21:47 -0300

    Adição dos projetos de teste e da classe de testes unitarios.

---
+++ APIComprasTest/CompraServiceFake.cs
@@ -0,0 +1,93 @@
+﻿using APICompras.Models;
+using APICompras.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace APIComprasTest
+{
+    class CompraServiceFake : ICompraService
+    {
+        public readonly List<CompraItem> _compra; //Convenção MS: "variáveis globais devem começar com '_'"
+
+        #region Construtor
+
+        public CompraServiceFake()
+        {
+            _compra = new List<CompraItem>() {
+
+                new CompraItem()
+                {
+                    Id = 1,
+                    Nome = "Moto One Action",
+                    Fabricante = "Motorola",
+                    Preco = 150.00M
+                },
+
+                new CompraItem()
+                {
+                    Id = 2,
+                    Nome = "Iphone 11",
+                    Fabricante = "Apple",
+                    Preco = 600.00M
+                },
+
+                new CompraItem()
+                {
+                    Id = 3,
+                    Nome = "Notebook Lenovo 13",
+                    Fabricante = "Lenovo",
+                    Preco = 1350.00M
+                },
+
+                new CompraItem()
+                {
+                    Id = 4,
+                    Nome = "Monitor LG 23",
+                    Fabricante = "LG",
+                    Preco = 879.00M
+                },
+
+                new CompraItem()
+                {
+                    Id = 5,
+                    Nome = "HD SSD Asus 1",
+                    Fabricante = "Asus",
+                    Preco = 600.00M
+                }
+            };
+        }
+
+        #endregion Construtor
+
+        public CompraItem Add(CompraItem novoItem)
+        {
+            novoItem.Id = GeraId();
+            _compra.Add(novoItem);
+            return novoItem;
+        }
+
+        public IEnumerable<CompraItem> GetAllItens()
+        {
+            return _compra;
+        }
+
+        public CompraItem GetById(int id)
+        {
+            return _compra.Where(a => a.Id == id).FirstOrDefault();
+        }
+
+        public void Remove(int id)
+        {
+            var item = _compra.First(a => a.Id == id);
+            _compra.Remove(item);
+        }
+
+        private int GeraId()
+        {
+            Random random = new Random();
+            return random.Next(1, 100);
+        }
+    }
+}
---
+++ APIComprasTest/ComprasControllerTest.cs
@@ -0,0 +1,42 @@
+﻿using APICompras.Controllers;
+using APICompras.Models;
+using APICompras.Services;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using Xunit;
+
+namespace APIComprasTest
+{
+    public class ComprasControllerTest
+    {
+        ComprasController _controller;
+        ICompraService _service;
+
+        public ComprasControllerTest()
+        {
+            _service = new CompraServiceFake();
+            _controller = new ComprasController(_service);
+        }
+
+        [Fact]
+        public void Get_WhenCalled_ReturnsOkResult()
+        {
+            //Act
+            var okResult = _controller.Get();
+
+            //Assert
+            Assert.IsType<OkObjectResult>(okResult.Result);
+        }
+
+        [Fact]
+        public void Get_WhenCalled_ReturnsAllItens()
+        {
+            //Act
+            var okResult = _controller.Get().Result as OkObjectResult;
+
+            //Assert
+            var items = Assert.IsType<List<CompraItem>>(okResult.Value);
+            Assert.Equal(5, items.Count);
+        }
+    }
+}
